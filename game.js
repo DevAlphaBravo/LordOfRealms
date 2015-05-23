251cb79f6325055fcedb913578c7980e8aed7a56
Kyle <dsichatter@gmail.com>
2015-05-23 10:40:21 -0700

    v0.0.2

--- game.js
+++ game.js
@@ -1,1 +1,24 @@
-alert("Test");
+var map = "map-0";
+var SOURCE;
+
+window.onload = function() {
+    Crafty.init(640,480);
+    Crafty.canvas.init();
+
+    //Loading Scene
+    Crafty.scene("loading", function() {
+        loading();
+    });
+    Crafty.scene("loading");
+}
+
+function loading() {
+    var textSize = 20;
+
+    Crafty.background("black");
+    Crafty.e("2D, DOM, Text")
+        .attr({x:0, y:232, w:640, h: 20})
+        .css({"text-align": "center"})
+        .textFont({size: "20px"})
+        .textColor("white");
+}
