04b0f997f0ef1115afde10feeac1d79767701a5e
Kyle <dsichatter@gmail.com>
2015-05-23 10:33:34 -0700

    v0.0.2

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-The servers will be hosted here. Sweet!
+<html>
+<head>
+    <title>Lord Of Realms</title>
+    <script type="text/javascript" src="libs/crafty-min.js"></script>
+    <script src="create_mocks_module.js"></script>
+    <script src="libs/tiledmapbuilder.js"></script>
+    <script type="text/javascript" src="game.js"></script>
+</head>
+<body>
+
+</body>
+</html>
