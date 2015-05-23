175a558e1be3bb750000bee2151153d54073fa89
Kyle <dsichatter@gmail.com>
2015-05-23 11:10:12 -0700

    v0.0.6

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         }
     </style>
     <script type="text/javascript" src="libs/crafty-min.js"></script>
-    <script src="create_mocks_module.js"></script>
+    <script src="libs/create_mocks_module.js"></script>
     <script src="libs/tiledmapbuilder.js"></script>
     <script type="text/javascript" src="game.js"></script>
 </head>
