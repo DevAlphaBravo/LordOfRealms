40ed962064a342341fd82b9d9b3769ffa1fda72e
Kyle <dsichatter@gmail.com>
2015-05-28 14:52:09 -0700

    v1.0.1

--- menu/index.php
+++ menu/index.php
@@ -5,5 +5,22 @@
         background:url(/images/bg.png);
         text-align:center;
     }
+
+    .input {
+        background:url(/images/form_bg.png) repeat-x;
+        border:1px solid #d1c7ac;
+        width: 230px;
+        color:#333333;
+        padding:3px;
+        margin-right:4px;
+        margin-bottom:8px;
+        font-family:tahoma, arial, sans-serif;
+    }
 </style>
 <img src="/images/logo.png" style="width:99%; margin:auto;">
+<br>
+<div style="width:75%; font-size:12px; text-align:center; margin:auto; background:white; padding:10px;">
+    <form action="login.php" method="post">
+        <input type="text" class="input" style="width:96%;">
+    </form>
+</div>
