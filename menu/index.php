766610e2128131242be2eb1af6e521a396c54289
Kyle <dsichatter@gmail.com>
2015-05-28 14:43:40 -0700

    v1.0.1

--- menu/index.php
+++ menu/index.php
@@ -1,1 +1,9 @@
 <title>Lord of Realms</title>
+<style>
+    body {
+        margin:0px;
+        background:url(/images/bg.png);
+        text-align:center;
+    }
+</style>
+<img src="/images/logo.png" style="width:99%; margin:auto;">
