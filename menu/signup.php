8d8454629865caccdb8b25a6ebe9ee5b96e42ec0
Kyle <dsichatter@gmail.com>
2015-05-29 07:36:51 -0700

    v1.0.1.1

--- menu/signup.php
+++ menu/signup.php
@@ -1,3 +1,76 @@
+<?php
+include "../includes/db.php";
+
+if(isset($_COOKIE['session'])) {
+    die("<script>window.location = '/';</script>");
+}
+
+function cleanString($txt) {
+    return htmlspecialchars($txt, ENT_QUOTES, 'UTF-8');
+}
+
+if(isset($_POST['username'])) {
+    $username = cleanString($_POST['username']);
+    $email = cleanString($_POST['email']);
+    $password = cleanString($_POST['password']);
+    $confirm = cleanString($_POST['confirm']);
+    $char = cleanString($_POST['character']);
+
+    $charCheck = false;
+
+    $etxt = "";
+
+    if($username == "") {
+        $etxt = "Please enter an username.";
+    }
+
+    if($email == "") {
+        $etxt = "Please enter an email.";
+    }
+
+    if($password == "") {
+        $etxt = "Plaes enter a password.";
+    }
+
+    switch($char) {
+        case "jeff":
+            $charCheck = true;
+            break;
+        case "amia":
+            $charCheck = true;
+            break;
+        case "jack":
+            $charCheck = true;
+            break;
+    }
+
+    if($charCheck == false) {
+        $etxt = "You did not select a valid character.";
+    }
+
+    $aon = query("SELECT * FROM members WHERE username=:username",array(
+        "username" => $username
+    ));
+    if($aon->rowCount() > 0) {
+        $etxt = "That username has already been taken.";
+    }
+
+    $bon = query("SELECT * FROM members WHERE email=:email",array(
+        ":email" => $email
+    ));
+    if($bon->rowCount() > 0) {
+        $etxt = "That email is already in our system.";
+    }
+
+    if($password != $confirm) {
+        $etxt = "Your passwords do not match.";
+    }
+
+    if($etxt == "") {
+
+    }
+}
+?>
 <title>Lord of Realms : Signup</title>
 <meta name="viewport" content="width=device-width">
 
@@ -20,6 +93,22 @@
         overflow:hidden;
         border:1px solid black;
     }
+    .button-green {
+        background: #2ba812;
+        background-image: -webkit-linear-gradient(top, #2ba812, #2a7a0b);
+        background-image: -moz-linear-gradient(top, #2ba812, #2a7a0b);
+        background-image: -ms-linear-gradient(top, #2ba812, #2a7a0b);
+        background-image: -o-linear-gradient(top, #2ba812, #2a7a0b);
+        background-image: linear-gradient(to bottom, #2ba812, #2a7a0b);
+        -webkit-border-radius: 28;
+        -moz-border-radius: 28;
+        border-radius: 28px;
+        font-family: Arial;
+        color: #ffffff;
+        font-size: 20px;
+        padding: 10px 20px 10px 20px;
+        text-decoration: none;
+    }
 </style>
 <script type="text/javascript">
     function selectCharacter(id) {
@@ -35,7 +124,7 @@
 <br>
 <div style="width:300px; background:white; text-align:center; font-size:12px; margin:auto; border:1px outset gray;">
     <h1 style="text-shadow:1px 0px 1px #888; color:darkgray;">Sign Up Form</h1>
-    <div style="font-size:10px; text-align:center;"></div>
+    <div style="font-size:10px; text-align:center;color:red;"><?php echo $etxt; ?></div>
     <form action="signup.php" method="post">
         <input type="text" class="input" name="username" placeholder="Desired Username"><br>
         <input type="text" class="input" name="email" placeholder="Email Address"><br>
@@ -47,7 +136,9 @@
             <tr>
                 <td><div id="jeff" name="characterSelection" class="selection" style="background:url(/characters/jeff.png) -36px -96px; width:36px; height:48px;" onclick="selectCharacter(this.id);"></div></td>
                 <td><div id="amia" name="characterSelection" class="selection" style="background:url(/characters/amia.png) -32px 0px; width:32px; height:32px;" onclick="selectCharacter(this.id);"></div></td>
+                <td><div id="jack" name="characterSelection" class="selection" style="background:url(/characters/jack.png) -32px 0px; width:32px; height:32px;" onclick="selectCharacter(this.id);"></div></td>
             </tr>
-        </table>
+        </table><br>
+        <input type="submit" class="button-green" value="Create Account">
     </form>
 </div>
