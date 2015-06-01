d2f5976ff49da6f75e165a555e6da8ca6e7a2f52
Kyle <dsichatter@gmail.com>
2015-06-01 16:30:38 -0700

    1.0

--- index.php
+++ index.php
@@ -14,6 +14,10 @@
 <body>
 <div class="uk-height-1-1 uk-width-1-1" style="overflow:hidden;">
     <img src="/images/bg.png" class="uk-height-1-1 uk-width-1-1" alt="">
+
+    <nav class="uk-nav">
+        <a href="/" class="uk-nav-brand"><img src="/images/logo.png" alt="" style="width:45%;"></a>
+    </nav>
 </div>
 </body>
 </html>
