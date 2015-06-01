371f199facc0e2675368d88be1a84fead3a13fc4
Kyle <dsichatter@gmail.com>
2015-06-01 16:11:39 -0700

    1.0

--- index.php
+++ index.php
@@ -5,12 +5,19 @@
     <?php
     require "includes/libs.php";
     ?>
+    <style>
+        body {
+            margin:0;
+        }
+    </style>
 </head>
 <body>
-<form class="uk-panel uk-panel-box uk-form">
-    <div class="uk-form-row">
-        <input class="uk-form-large" type="text" placeholder="Username">
+<div class="uk-vertical-align-middle uk-text-center" style="width:300px;">
+    <form action="menu/login.php" method="post" class="uk-panel uk-panel-box uk-form">
+        <div class="uk-form-row">
+            <input type="text" class="uk-form-large" placeholder="Username">
+        </div>
+        </form>
     </div>
-</form>
 </body>
 </html>
