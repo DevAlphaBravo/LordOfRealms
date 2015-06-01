05ceeb5cf95f4ad92b3a648282725582fea93b68
Kyle <dsichatter@gmail.com>
2015-06-01 16:46:47 -0700

    1.0

--- includes/libs.php
+++ includes/libs.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="/css/uikit.min.css">
+<link rel="stylesheet" href="/css/uikit.gradient.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="/js/uikit.gradient.min.js"></script>
+<script src="/js/uikit.min.js"></script>
--- index.php
+++ index.php
@@ -14,10 +14,15 @@
 <body>
 <div class="uk-height-1-1 uk-width-1-1" style="overflow:hidden;background:url(/images/bg.png);">
     <nav class="uk-navbar">
-        <a href="/" class="uk-navbar-brand"><img src="/images/logo.png" alt="" style="width:10%;"></a>
-        <ul class="uk-navbar-nav">
-            <li><a href="/">Home</a></li>
-        </ul>
+        <a href="/" class="uk-navbar-brand"><img src="/images/logo.png" alt="" style="width:20%;"></a>
+        <div class="uk-navbar-flip">
+            <div class="uk-navbar-content">
+                <div class="uk-button-group">
+                    <button class="uk-button-primary" onclick="window.location = '/menu/login.php';">Login</button>
+                    <button class="uk-button-primary" onclick="window.location = '/menu/register.php';">Register</button>
+                </div>
+            </div>
+        </div>
     </nav>
 </div>
 </body>
