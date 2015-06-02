37908be9d07723be27b98a322995f736a1727220
Kyle <dsichatter@gmail.com>
2015-06-01 17:04:11 -0700

    1.0

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <div class="uk-height-1-1 uk-width-1-1" style="overflow:hidden;background:url(/images/bg.png);">
     <nav class="uk-navbar">
         <div class="uk-navbar-content"><a href="/"><img src="/images/logo.png" style="width:20%;" alt=""></a></div>
-        <div class="uk-navbar-content uk-navbar-flip uk-hidden-small">
+        <div class="uk-navbar-content uk-navbar-flip">
             <div class="uk-button-group">
                 <a href="/menu/login.php" class="uk-button">Login</a>
                 <button class="uk-button uk-button-primary" onclick="window.location = '/menu/register.php';">Register</button>
